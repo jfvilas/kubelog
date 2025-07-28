9f7d4b66099351c449f5615f07f2f399e027f577
Julio Fernandez <jfvilas@outlook.com>
2025-07-28 12:58:53 +0200

    change git config

--- src/plugin.ts
+++ src/plugin.ts
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 import { kubelogApiRef, KubelogClient } from './api';
 import { createApiFactory, createPlugin, createRoutableExtension } from '@backstage/core-plugin-api';
 import { discoveryApiRef, fetchApiRef } from '@backstage/core-plugin-api';
